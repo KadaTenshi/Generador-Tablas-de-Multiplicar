a853b0c1e8e0d5c487b563eb7d55319f27e610e0
KadaTenshi <andersontorres.2004may@gmail.com>
2024-10-07 20:17:29 -0500

    Add files via upload

---
+++ Multiplicar.js
@@ -0,0 +1,8 @@
+// Tabla de multiplicar de un número fijo
+let numero = 5; 
+
+// Utiliza un ciclo for para generar la tabla de multiplicar
+console.log(`Tabla de multiplicar del ${numero}:`);
+for (let i = 1; i <= 10; i++) {
+  console.log(`${numero} x ${i} = ${numero * i}`);
+}
